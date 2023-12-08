6583bba3a25e57c32c8b88645d474a1fc610ac48
gkluber <gkluber@sbcglobal.net>
2023-12-08 21:33:12 +1100

    finished day 8... think I'm going to move to solving these on notebooks

--- lib/day8.ex
+++ lib/day8.ex
@@ -14,21 +14,32 @@ defmodule Day8 do
     )
   end
 
-  def solution1(lines) do
-    [instructions | [""] ++ lines] = lines
+  defp parse_input(lines) do
+    [dirs | [""] ++ lines] = lines
     formatted = lines |> Enum.map(&String.replace(&1, " ", ""))
     eqs = formatted |> Enum.map(fn line ->
       {:ok, values, _, _, _, _} = Parser.parse(line)
       values
     end)
-    wobblers = eqs |> Enum.reduce(%{}, fn [source, left, right], acc ->
+    forks = eqs |> Enum.reduce(%{}, fn [source, left, right], acc ->
       Map.put(acc, source, {left, right})
     end)
 
-    instructions = Stream.cycle(instructions |> String.graphemes) #infinitely cycle/loop through instructions
+    #infinitely cycle/loop through instructions
+    dirs = Stream.cycle(dirs |> String.graphemes)
 
-    instructions |> Enum.reduce_while({1, "AAA"}, fn dir, {num, cur} ->
-      {left, right} = Map.get(wobblers, cur)
+    {dirs, forks}
+  end
+
+  defp lcm(a,b) do
+    div(a*b, Integer.gcd(a,b))
+  end
+
+  def solution1(lines) do
+    {dirs, forks} = parse_input(lines)
+
+    dirs |> Enum.reduce_while({1, "AAA"}, fn dir, {num, cur} ->
+      {left, right} = Map.get(forks, cur)
       next = case dir do
         "L" -> left
         "R" -> right
@@ -39,5 +50,34 @@ defmodule Day8 do
         {:cont, {num+1, next}}
       end
     end)
+  end
+
+  def solution2(lines) do
+    {dirs, forks} = parse_input(lines)
+    as = Map.keys(forks) |> Enum.filter(&String.ends_with?(&1, "A"))
+    zs = Map.keys(forks) |> Enum.filter(&String.ends_with?(&1, "Z"))
+
+    cycles = Enum.map(as, fn seed ->
+      dirs |> Enum.reduce_while({1, seed}, fn dir, {num, cur} ->
+        {left, right} = Map.get(forks, cur)
+        next = case dir do
+          "L" -> left
+          "R" -> right
+        end
+        if next in zs do
+          {:halt, num}
+        else
+          {:cont, {num+1, next}}
+        end
+      end)
+    end)
+
+    IO.puts(inspect cycles)
+
+    answer = Enum.reduce(cycles, 1, fn x, acc ->
+      lcm(x,acc)
+    end)
+
+    answer
   end
 end
